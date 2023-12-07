f4b772b791fe4051921c0570b541c232f0a75ec0
Hogwartman <Hogwartman@users.noreply.github.com>
2023-12-07 20:30:44 +0800

    堆栈式主要按钮功能赋予
    初步集合了多个页面于同一页面，并且点击按钮可以相互切换

---
+++ UI前端/unit_demo/unit_demo_launch.py
@@ -0,0 +1,80 @@
+#-*- codeing = utf-8 -*-
+#@Time : 2023/12/7 19:53
+#-*- codeing = utf-8 -*-
+#@Time : 2023/12/6 20:35
+
+from unit_demo import *
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+from PyQt5.QtWidgets import QMessageBox
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.show()
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and not self.isMaximized():
+            self.m_flag = True
+            self.m_Position = event.pos()  # 获取鼠标相对窗口位置
+            event.accept()
+            self.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
+
+    def mouseMoveEvent(self, mouse_event):
+        if mouse_event.buttons() == QtCore.Qt.LeftButton and self.m_flag:
+            self.move(self.mapToGlobal(mouse_event.pos() - self.m_Position))  # 更改窗口位置
+            mouse_event.accept()
+
+    def mouseReleaseEvent(self, mouse_event):
+        self.m_flag = False
+        self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+
+    def on_signin_button_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
+    def on_account_had_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+
+    def on_Bt_main_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+    def on_Bt_exercise_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+    def on_Bt_wrong_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+    def on_Bt_setting_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_Bt_main_1_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+    def on_Bt_exercise_1_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+    def on_Bt_wrong_1_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+    def on_Bt_setting_1_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_Bt_main_2_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+    def on_Bt_exercise_2_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+    def on_Bt_wrong_2_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+    def on_Bt_setting_2_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_Bt_main_3_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+    def on_Bt_exercise_3_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+    def on_Bt_wrong_3_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+    def on_Bt_setting_3_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = LoginWindow()
+    sys.exit(app.exec_())
